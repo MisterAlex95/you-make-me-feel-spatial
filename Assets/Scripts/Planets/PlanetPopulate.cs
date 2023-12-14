d76358b9c009d52d4e252c40508979b03bb61285
nixael <mmunhurrun@pixaelstudio.com>
2023-12-14 16:41:33 +0100

    test populate

--- Assets/Scripts/Planets/PlanetPopulate.cs
+++ Assets/Scripts/Planets/PlanetPopulate.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vector3 = System.Numerics.Vector3;
 
 public class PlanetPopulate : MonoBehaviour
 {
@@ -11,11 +10,35 @@ public class PlanetPopulate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 360; i += 15)
+        for (int i = 0; i < 360; i += 0)
         {
-            //var go = Instantiate(populateItem, transform);
-            //go.transform.RotateAround(transform.position, Vector2.right, i);
+            var go = Instantiate(populateItem, transform);
+            var randomScale = Random.Range(0.15f, 0.25f);
+            var v = go.transform.position;
+            v.z = 0.5f - randomScale;
+            v.y -= (0.25f - randomScale);
+            go.transform.position = v;
+            go.transform.localScale = new Vector3(randomScale, randomScale);
+            go.transform.RotateAround(transform.position, Vector3.forward, i);
+            var randomSprite = Random.Range(0, sprites.Length);
+            go.GetComponent<SpriteRenderer>().sprite = sprites[randomSprite];
+
+            var range = 1 / (0.25f / randomScale);
+
+            if (randomScale >= 0.15f && randomScale < 0.18f)
+            {
+                go.GetComponent<SpriteRenderer>().color = new Color(0.35f , 0.35f, 0.35f);
+            }
+            if (randomScale >= 0.18f && randomScale < 0.22f)
+            {
+                go.GetComponent<SpriteRenderer>().color = new Color(0.6f , 0.6f, 0.6f);
+            }
+            if (randomScale >= 0.22f)
+            {
+                go.GetComponent<SpriteRenderer>().color = new Color(0.8f , 0.8f, 0.8f);
+            }
             
+            i += Random.Range(15, 20);
         }
     }
 
