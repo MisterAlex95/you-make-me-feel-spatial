0b6dc68686056de487d18c56d8e4a5222a8de09c
Alex Dana <alex.dana@ubisoft.com>
2023-12-15 11:56:52 +0100

    some fiexes

--- Assets/Scripts/UI/NewEraganeLetter.cs
+++ Assets/Scripts/UI/NewEraganeLetter.cs
@@ -22,6 +22,7 @@ namespace UI
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            HoverAsset.SetActive(false);
             _newErganeCodexPage.GoToLetter(this.letter);
         }
 
